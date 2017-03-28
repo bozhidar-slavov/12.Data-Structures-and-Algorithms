e6f0b5be697eafb8d7dcb90a4de131807f60838a
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2017-03-28 18:33:23 +0300

    Advanced Data Structures tasks added!

---
+++ 10.Advanced-Data-Structures/PriorityQueue/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace PriorityQueue
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            PriorityQueue<string> queue = new PriorityQueue<string>();
+            queue.Enqueue("Kiro");
+            queue.Enqueue("Unufri");
+            queue.Enqueue("Mimi");
+            queue.Enqueue("Penka");
+            queue.Enqueue("Gosho");
+            queue.Enqueue("Zoro");
+
+            Console.WriteLine($"The root: {queue.Root}");
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
---
+++ 10.Advanced-Data-Structures/SetOfWords/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace SetOfWords
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Triepocalypse;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Trie<int> trie = new Trie<int>();
+
+            using (StreamReader reader = new StreamReader("..\\..\\input.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    reader
+                        .ReadLine()
+                        .Split(' ', '.', ',', '?', '!', ':')
+                        .ToList()
+                        .ForEach(word =>
+                        {
+                            if (!trie.ContainsKey(word))
+                            {
+                                trie.Add(word, 1);
+                            }
+                            else
+                            {
+                                trie[word] += 1;
+                            }
+                        });
+                }
+            }
+
+            trie.Matcher.Next("lorem");
+
+            int countOfLorem = 0;
+
+            if (trie.Matcher.IsMatch())
+            {
+                countOfLorem = trie.Matcher.GetExactMatch();
+            }
+
+            Console.WriteLine(countOfLorem);
+        }
+    }
+}
