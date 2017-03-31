0acc3611dfedfd4c2213d010459d5b46e0def80c
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2017-03-31 16:22:10 +0300

    Added some tasks with data structures!

---
+++ 11.Data-Structures-Efficiency/01.StudentsAndCourses/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace StudentsAndCourses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var dictionary = new SortedDictionary<string, SortedSet<Student>>();
+
+            ReadStudentsInfoFromFile("../../input.txt", dictionary);
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0}: {1}", item.Key, string.Join(", ", item.Value));
+            }
+        }
+
+        private static void ReadStudentsInfoFromFile(string filePath, SortedDictionary<string, SortedSet<Student>> courses)
+        {
+            var reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while (line != string.Empty && line != null)
+                {
+                    var splitted = line.Split(new char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    string firstName = splitted[0];
+                    string lastName = splitted[1];
+                    string courseName = splitted[2];
+
+                    if (courses.ContainsKey(courseName))
+                    {
+                        courses[courseName].Add(new Student(firstName, lastName));
+                    }
+                    else
+                    {
+                        courses[courseName] = new SortedSet<Student>();
+                        courses[courseName].Add(new Student(firstName, lastName));
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ 11.Data-Structures-Efficiency/03.BiDictionary/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace BiDictionary
+{
+    using System;
+    using System.IO;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            IBiDictionary<string, int, string> dictionary = new BiDictionary<string, int, string>();
+            
+            ReadDataFromFile("../../input.txt", dictionary);
+            
+            var searchByFirstKey = dictionary.SearchByFirstKey("Plovdiv");
+            Console.WriteLine("Search by name:");
+            Console.WriteLine(string.Join(", ", searchByFirstKey));
+            Console.WriteLine();
+
+            var searchBySecondKey = dictionary.SearchBySecondKey(23);
+            Console.WriteLine("Search by age:");
+            Console.WriteLine(string.Join(", ", searchBySecondKey));
+            
+            var serarchByTwoKeys = dictionary.SearchByTwoKeys("Plovdiv", 20);
+            Console.WriteLine("Search by sity and age:");
+            Console.WriteLine(string.Join(", ", serarchByTwoKeys));
+            Console.WriteLine();
+        }
+
+        private static void ReadDataFromFile(string filePath, IBiDictionary<string, int, string> dictionary)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line = reader.ReadLine();
+                while (line != string.Empty && line != null)
+                {
+                    string[] data = line.Split('|');
+                    string name = data[0].Trim();
+                    string city = data[1].Trim();
+                    int age = int.Parse(data[2].Trim());
+
+                    dictionary.Add(city, age, name);
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
